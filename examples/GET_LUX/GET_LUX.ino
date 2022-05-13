94d1419976a98e871996014aa8ac42b42295b4d6
Tinyu <Tinyu.Zhao@gmail.com>
2022-05-13 14:15:57 +0800

    update and format

--- examples/GET_LUX/GET_LUX.ino
+++ examples/GET_LUX/GET_LUX.ino
@@ -1,4 +1,15 @@
-
+/*
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with M5StickC sample source code
+*                          配套  M5StickC 示例源代码
+* Visit for more information：https://docs.m5stack.com/en/hat/hat_dlight
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/hat/hat_dlight
+*
+* describe：lux.
+* date：2022/5/13
+*******************************************************************************
+*/
 #include <M5StickC.h>
 #include <M5_BH1750FVI.h>
 
@@ -8,32 +19,32 @@ M5_BH1750FVI sensor;
 uint16_t lux;
 
 void setup() {
-  M5.begin();
-  M5.Lcd.setRotation(1);
-  canvas.setTextColor(GREEN);
-  canvas.setTextDatum(MC_DATUM);
-  canvas.setTextSize(2);
-  canvas.createSprite(160, 80);
-  Serial.println("Sensor begin.....");
-  sensor.begin();
+    M5.begin();
+    M5.Lcd.setRotation(1);
+    canvas.setTextColor(GREEN);
+    canvas.setTextDatum(MC_DATUM);
+    canvas.setTextSize(2);
+    canvas.createSprite(160, 80);
+    Serial.println("Sensor begin.....");
+    sensor.begin();
 
-  //CONTINUOUSLY_H_RESOLUTION_MODE
-  //CONTINUOUSLY_H_RESOLUTION_MODE2
-  //CONTINUOUSLY_L_RESOLUTION_MODE
-  //ONE_TIME_H_RESOLUTION_MODE
-  //ONE_TIME_H_RESOLUTION_MODE2
-  //ONE_TIME_L_RESOLUTION_MODE
-  sensor.setMode(CONTINUOUSLY_H_RESOLUTION_MODE);
+    // CONTINUOUSLY_H_RESOLUTION_MODE
+    // CONTINUOUSLY_H_RESOLUTION_MODE2
+    // CONTINUOUSLY_L_RESOLUTION_MODE
+    // ONE_TIME_H_RESOLUTION_MODE
+    // ONE_TIME_H_RESOLUTION_MODE2
+    // ONE_TIME_L_RESOLUTION_MODE
+    sensor.setMode(CONTINUOUSLY_H_RESOLUTION_MODE);
 }
 
 char info[40];
 
 void loop() {
-  lux = sensor.getLUX();
-  sprintf(info, "lux: %d", lux);
-  canvas.fillSprite(BLACK);
-  canvas.drawString(info, 80, 40);
-  canvas.pushSprite(0, 0);
-  Serial.println(info);
-  delay(100);
+    lux = sensor.getLUX();
+    sprintf(info, "lux: %d", lux);
+    canvas.fillSprite(BLACK);
+    canvas.drawString(info, 80, 40);
+    canvas.pushSprite(0, 0);
+    Serial.println(info);
+    delay(100);
 }
--- src/M5_BH1750FVI.h
+++ src/M5_BH1750FVI.h
@@ -1,41 +1,43 @@
 #ifndef __M5_BH1750FVI_H__
 #define __M5_BH1750FVI_H__
- 
+
 #include "Arduino.h"
 #include "Wire.h"
+#include "pins_arduino.h"
 
 #define DEVICE_I2C_ADDR 0x23
 
-//CMD
-#define POWER_DOWN B00000000
-#define POWER_ON B00000001
-#define RESET B00000111
-#define CONTINUOUSLY_H_RESOLUTION_MODE B00010000
+// CMD
+#define POWER_DOWN                      B00000000
+#define POWER_ON                        B00000001
+#define RESET                           B00000111
+#define CONTINUOUSLY_H_RESOLUTION_MODE  B00010000
 #define CONTINUOUSLY_H_RESOLUTION_MODE2 B00010001
-#define CONTINUOUSLY_L_RESOLUTION_MODE B00010011
-#define ONE_TIME_H_RESOLUTION_MODE B00100000
-#define ONE_TIME_H_RESOLUTION_MODE2 B00100001
-#define ONE_TIME_L_RESOLUTION_MODE B00100011
+#define CONTINUOUSLY_L_RESOLUTION_MODE  B00010011
+#define ONE_TIME_H_RESOLUTION_MODE      B00100000
+#define ONE_TIME_H_RESOLUTION_MODE2     B00100001
+#define ONE_TIME_L_RESOLUTION_MODE      B00100011
 
 class M5_BH1750FVI {
+   private:
+    TwoWire *_wire;
+    uint8_t _sda;
+    uint8_t _scl;
+    uint32_t _freq;
+    uint8_t _addr;
+    void writeByte(byte cmd);
+    void writeBytes(uint8_t *buffer, size_t size);
+    void readBytes(uint8_t *buffer, size_t size);
 
-      private:
-            TwoWire * _wire;
-            uint8_t _sda;
-            uint8_t _scl;
-            uint32_t _freq; 
-            uint8_t _addr;
-            void writeByte(byte cmd);
-		void writeBytes(uint8_t *buffer, size_t size);
-            void readBytes(uint8_t *buffer, size_t size);
-      public:
-            M5_BH1750FVI(uint8_t addr = 0x23);
-            void begin(TwoWire *wire = &Wire, uint8_t sda = 0, uint8_t scl = 26, uint32_t freq = 4000000);
-            void powerOn();
-            void powerOff();
-            void powerReset();
-            void setMode(byte mode);
-            uint16_t getLUX();
+   public:
+    M5_BH1750FVI(uint8_t addr = 0x23);
+    void begin(TwoWire *wire = &Wire, uint8_t sda = SDA, uint8_t scl = SCL,
+               uint32_t freq = 4000000UL);
+    void powerOn();
+    void powerOff();
+    void powerReset();
+    void setMode(byte mode);
+    uint16_t getLUX();
 };
 
 #endif
